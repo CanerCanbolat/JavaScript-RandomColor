8359f5abbde625fef868a6284dc1525a7e60094c
Caner Canbolat <107541646+CanerCanbolat@users.noreply.github.com>
2024-03-12 10:58:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+  const newColor = makeRandColor();
+  document.body.style.backgroundColor = newColor;
+  h1.innerText = newColor;
+});
+
+const makeRandColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  if (r < 40 && g < 40 && b < 70) {
+    h1.style.color = `white`;
+  } else {
+    h1.style.color = "black";
+  }
+  return `rgb(${r}, ${g}, ${b})`;
+};
